1ebf0bd69e45b1a01a9e2be776050753563936bd
Kaique Souza Costa <105029198+Kaiquey@users.noreply.github.com>
2022-06-14 22:05:22 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,11 +4,11 @@
 				<p class = "float-end mb-1">
 					<a href="#"><i class="fa-solid fa-arrow-up-long"></i></i></a></p>
 				<p class = "mb-1"> Copyright  &copy 2022. Todos os direitos Reservados.</p
-				<p class="mb-0"><a href="#"><i class="fa-brands fa-whatsapp"></i></a>
-								<a href="#"><i class="fa-brands fa-github"></i></a>	
-								<a href="#"><i class="fa-brands fa-facebook"></i></a>
-								<a href="#"><i class="fa-brands fa-instagram"></i></a>
-								<a href="#"><i class="fa-brands fa-linkedin"></i></a>
+				<p class="mb-0"><a href="wa.me/+5571986437681" target ="_blanck"><i class="fa-brands fa-whatsapp"></i></a>
+								<a href="https://github.com/Kaiquey" target ="_blanck"><i class="fa-brands fa-github"></i></a>	
+								<a href="#"><i class="fa-brands fa-facebook" target ="_blanck"></i></a>
+								<a href="https://www.instagram.com/myerz.xp/" target ="_blanck"><i class="fa-brands fa-instagram"></i></a>
+								<a href="https://www.linkedin.com/in/kaique-souza-costa-40181522a/" target ="_blanck"><i class="fa-brands fa-linkedin"></i></a>
 			</div>
 		</footer>
 </body>
